87dde4f20adf062416cb323e093d9f8f06102ce2
Tinapat Game Limsila <limsila.limsila@gmail.com>
2025-02-11 20:29:31 +0700

    working interface with Dyanmixel

---
+++ hardware/dynamixel_class.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys, tty, termios
+from dynamixel_sdk import * # Uses Dynamixel SDK library
+from math import cos, radians, degrees
+import numpy
+# DYNAMIXEL Protocol Version (1.0 / 2.0)
+# https://emanual.robotis.com/docs/en/dxl/protocol2/
+PROTOCOL_VERSION            = 1.0 # Need this for AX-12
+
+# Use the actual port assigned to the U2D2.
+# ex) Windows: "COM*", Linux: "/dev/ttyUSB*", Mac: "/dev/tty.usbserial-*"
+# DEVICENAME                  = '/dev/ttyUSB0'
+# DEVICENAME                  = '/dev/tty.usbmodem101'
+# sudo chmod 777 /dev/tty.usbserial-AB0NRMLW # USE THIS FOR MAC & LINUX to allow port
+DEVICENAME = '/dev/tty.usbserial-AB0NRMLW'  # Update this with your ports
+
+DISABLE = 0
+ENABLE = 1
+DXL_MOVING_STATUS_THRESHOLD = 20    # Dynamixel moving status threshold
+
+index = 0
+dxl_goal_speed = [300, 800]         # Goal position
+
+#********* DYNAMIXEL Model definition *********
+MY_DXL = 'AX-12'
+
+# Control table address
+if MY_DXL == 'AX-12':
+    # ADDR_TORQUE_ENABLE          = 64
+    # ADDR_GOAL_POSITION          = 116
+    # ADDR_PRESENT_POSITION       = 132
+    ADDR_TORQUE_ENABLE          = 24
+    ADDR_GOAL_POSITION          = 30
+    ADDR_MOVING_SPEED           = 32
+    ADDR_TORQUE_LIMIT           = 34
+    ADDR_PRESENT_POSITION       = 36
+    DXL_MINIMUM_POSITION_VALUE  = 100   # Refer to the Minimum Position Limit of product eManual
+    DXL_MAXIMUM_POSITION_VALUE  = 1000    # Refer to the Maximum Position Limit of product eManual
+    BAUDRATE                    = 1000000
+
+
+# Class
+class DynamixelInterface:
+    def __init__(self, device_name=DEVICENAME, protocol_version=PROTOCOL_VERSION):
+        self.portHandler    = PortHandler(device_name)            # Initialize PortHandler instance
+        self.packetHandler  = PacketHandler(protocol_version)    # Initialize PacketHandler instance
+        self.motors_id = [1,2,3,4]
+        # Open port
+        if self.portHandler.openPort():
+            print("Succeeded to open the port")
+        else:
+            print("Failed to open the port")
+            quit()
+
+        # Set port baudrate
+        if self.portHandler.setBaudRate(BAUDRATE):
+            print("Succeeded to change the baudrate")
+        else:
+            print("Failed to change the baudrate")
+            quit()
+
+    def write_register(self, dxl_id, address, value, size=2):
+        if size == 1:
+            result, error = self.packetHandler.write1ByteTxRx(self.portHandler, dxl_id, address, value)
+        elif size == 2:
+            result, error = self.packetHandler.write2ByteTxRx(self.portHandler, dxl_id, address, value)
+        else:
+            raise ValueError("Unsupported register size")
+            
+        if result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(result))
+            return False
+        elif error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(error))
+            return False
+        return True
+
+    def read_register(self, dxl_id, address, size=2) -> int:
+        """
+        Read a register value from the Dynamixel
+        size: 1 for 1 byte registers, 2 for 2 byte registers
+        """
+        if size == 1:
+            value, result, error = self.packetHandler.read1ByteTxRx(self.portHandler, dxl_id, address)
+        elif size == 2:
+            value, result, error = self.packetHandler.read2ByteTxRx(self.portHandler, dxl_id, address)
+        else:
+            raise ValueError("Invalid size. Use 1 or 2")
+            
+        if result != COMM_SUCCESS:
+            print(f"Failed to read address {address}: {self.packetHandler.getTxRxResult(result)}")
+        elif error != 0:
+            print(f"Error reading address {address}: {self.packetHandler.getRxPacketError(error)}")
+        
+        return value
+
+    # Function to print key register values
+    def print_motor_state(self, dxl_id):
+        print("\n--- Motor State ---")
+        print(f"ID: {self.read_register(dxl_id, 3, size=1)}")
+        print(f"Baudrate: {self.read_register(dxl_id, 4, size=1)}")
+        print(f"CW Angle Limit: {self.read_register(dxl_id, 6)}")
+        print(f"CCW Angle Limit: {self.read_register(dxl_id, 8)}")
+        print(f"Torque Enable: {self.read_register(dxl_id, ADDR_TORQUE_ENABLE, size=1)}")
+        print(f"Moving Speed: {self.read_register(dxl_id, ADDR_MOVING_SPEED)}")
+        print(f"Torque Limit: {self.read_register(dxl_id, ADDR_TORQUE_LIMIT)}")
+        print(f"Present Position: {self.read_register(dxl_id, ADDR_PRESENT_POSITION)}")
+        print(f"Present Speed: {self.read_register(dxl_id, 38)}")  # Speed is at address 38
+        print(f"Present Load: {self.read_register(dxl_id, 40)}")
+        print("----------------\n")
+    
+    def enable_all_motors(self):
+        for dxl_id in self.motors_id:
+            self.write_register(dxl_id, ADDR_TORQUE_ENABLE, ENABLE, 1)
+        
+    def disable_all_motors(self):
+        for dxl_id in self.motors_id:
+            self.write_register(dxl_id, ADDR_TORQUE_ENABLE, DISABLE, 1)
+            
+    def set_moving_speed(self, dxl_id, speed=0, torque=500):
+        # check 0 <= torque <= 1023
+        if (torque < 0 or torque > 1023 or type(torque) != int):
+            raise ValueError
+        
+        # check 0 <= speed  <= 2047
+        if (speed < 0 or speed > 2047 or type(speed) != int):
+            raise ValueError(speed)
+        
+        self.write_register(dxl_id, ADDR_TORQUE_LIMIT, torque, 2)
+        self.write_register(dxl_id, ADDR_MOVING_SPEED, speed, 2)
+        print("speed: ", self.read_register(dxl_id ,ADDR_MOVING_SPEED, 2))
+        
+    
+    def testing_motors_moving_speed(self, speed1=300, speed2=1000):
+        try:
+            for dxl_id in self.motors_id:
+                print(dxl_id)
+                self.write_register(dxl_id, ADDR_TORQUE_LIMIT, 500, 2)
+            
+            for dxl_id in self.motors_id:
+                print(dxl_id)
+                # Write goal speed
+                self.write_register(dxl_id, ADDR_MOVING_SPEED, speed1, 2)
+                print("speed: ", self.read_register(dxl_id ,ADDR_MOVING_SPEED, 2))
+            time.sleep(4)
+                
+            for dxl_id in self.motors_id:
+                print(dxl_id)
+                self.write_register(dxl_id, ADDR_MOVING_SPEED, speed2, 2)
+                print("speed: ", self.read_register(dxl_id, ADDR_MOVING_SPEED, 2))
+            time.sleep(4)
+            self.disable_all_motors()
+        except KeyboardInterrupt:
+            self.disable_all_motors()
+            self.portHandler.closePort()
+            
+    def tf_speed(self, speed):
+        # input -1023 to 1023 for easy 
+        # return postive moving speed for memory address for both clockwise and anticlocker
+        if int(speed) < 0: # if -ive
+            return int(1024 - int(speed))
+        return int(speed)
+        
+        
+    def forward(self, speed=500, torque=1023):
+            interface.set_moving_speed(1, speed, torque)
+            interface.set_moving_speed(2, 1024+speed, torque)
+            interface.set_moving_speed(3, 1024+speed, torque)
+            interface.set_moving_speed(4, speed, torque)
+    
+    def right(self, speed=500, torque=1023):
+            interface.set_moving_speed(1, speed, torque)
+            interface.set_moving_speed(2, speed, torque)
+            interface.set_moving_speed(3, 1024+speed, torque)
+            interface.set_moving_speed(4, 1024+speed, torque)
+    
+    def backward(self, speed=500, torque=1023):
+            interface.set_moving_speed(1, 1024+speed, torque)
+            interface.set_moving_speed(2, speed, torque)
+            interface.set_moving_speed(3, speed, torque)
+            interface.set_moving_speed(4, 1024+speed, torque)
+    
+    def left(self, speed=500, torque=1023):
+            interface.set_moving_speed(1, 1024+speed, torque)
+            interface.set_moving_speed(2, 1024+speed, torque)
+            interface.set_moving_speed(3, speed, torque)
+            interface.set_moving_speed(4, speed, torque)
+    
+    def drive45(self, speed=500, torque=1023):
+            interface.set_moving_speed(1, self.tf_speed(speed*1), torque)
+            interface.set_moving_speed(2, self.tf_speed(0), torque)
+            interface.set_moving_speed(3, self.tf_speed(-speed*1), torque)
+            interface.set_moving_speed(4, self.tf_speed(), torque)
+            
+    def drive(self, angle=45, speed=500, torque=1023): 
+            speeds_trig = numpy.array( [+cos(radians(angle) - radians(45)), +cos(radians(angle) - radians(135)), -cos(radians(angle) - radians(45)), -cos(radians(angle) - radians(135))]) * speed
+            speeds = [self.tf_speed(i) for i in speeds_trig]
+            # print(speeds_trig)
+            print(speeds)
+            interface.set_moving_speed(1, speeds[0], torque)
+            interface.set_moving_speed(2, speeds[1], torque)
+            interface.set_moving_speed(3, speeds[2], torque)
+            interface.set_moving_speed(4, speeds[3], torque)
+    
+    def anticlockwise(self, speed=500, torque=1023):
+            interface.set_moving_speed(1, 1024+speed, torque)
+            interface.set_moving_speed(2, 1024+speed, torque)
+            interface.set_moving_speed(3, 1024+speed, torque)
+            interface.set_moving_speed(4, 1024+speed, torque)
+    
+        
+    def clockwise(self, speed=500, torque=1023):
+            interface.set_moving_speed(1, speed, torque)
+            interface.set_moving_speed(2, speed, torque)
+            interface.set_moving_speed(3, speed, torque)
+            interface.set_moving_speed(4, speed, torque)
+    
+            
+    def circular_clockwise(self, speed=500, torque=1023):
+        for i in range(180):
+            interface.drive(i*4)
+            time.sleep(0.01)
+
+if __name__ == "__main__":
+    try:
+        
+        interface = DynamixelInterface()
+        interface.disable_all_motors()
+        input("enter to start")
+        # interface.testing_motors_moving_speed()
+        # Forward
+        # (2) +1 /  \ -1 (3)
+        # (1) +1 \  / -1 (4)
+        # while 1:
+        #     x = int(input("input:speed"))
+        #     print(interface.set_moving_speed(2, speed=x, torque=500)) 
+        while 1:
+            interface.circular_clockwise(200, 200)
+            # interface.drive(45, 200)
+            # interface.forward()
+            time.sleep(1)
+            # interface.right()
+            # time.sleep(1)
+            # interface.backward() 
+            # time.sleep(1)
+            # interface.left()
+            # time.sleep(1)
+            # interface.anticlockwise()
+            # time.sleep(1)
+            # interface.clockwise()
+            # time.sleep(1)
+    except KeyboardInterrupt:
+        interface.disable_all_motors()
+        
+
+
+
+""" 
+# Control Table Address
+ADDR_MODEL_NUMBER          = 0
+ADDR_FIRMWARE_VERSION      = 2
+ADDR_ID                   = 3
+ADDR_BAUD_RATE            = 4
+ADDR_RETURN_DELAY_TIME    = 5
+ADDR_CW_ANGLE_LIMIT       = 6
+ADDR_CCW_ANGLE_LIMIT      = 8
+ADDR_TEMPERATURE_LIMIT    = 11
+ADDR_MIN_VOLTAGE_LIMIT    = 12
+ADDR_MAX_VOLTAGE_LIMIT    = 13
+ADDR_MAX_TORQUE          = 14
+ADDR_STATUS_RETURN_LEVEL  = 16
+ADDR_ALARM_LED           = 17
+ADDR_SHUTDOWN            = 18
+ADDR_TORQUE_ENABLE       = 24
+ADDR_LED                 = 25
+ADDR_CW_COMPLIANCE_MARGIN = 26
+ADDR_CCW_COMPLIANCE_MARGIN = 27
+ADDR_CW_COMPLIANCE_SLOPE  = 28
+ADDR_CCW_COMPLIANCE_SLOPE = 29
+ADDR_GOAL_POSITION       = 30
+ADDR_MOVING_SPEED        = 32
+ADDR_TORQUE_LIMIT        = 34
+ADDR_PRESENT_POSITION    = 36
+ADDR_PRESENT_SPEED       = 38
+ADDR_PRESENT_LOAD        = 40
+ADDR_PRESENT_VOLTAGE     = 42
+ADDR_PRESENT_TEMPERATURE = 43
+ADDR_REGISTERED          = 44
+ADDR_MOVING              = 46
+"""
