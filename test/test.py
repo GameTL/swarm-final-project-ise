5a35f424920859b80d0555ad6263af9446084e93
unknown <lin13445@gmail.com>
2025-03-26 00:41:22 +0700

    test

--- test/test.py
+++ test/test.py
@@ -34,4 +34,9 @@ def track(matrix_coefficients, distortion_coefficients):
     cap.release()
     cv2.destroyAllWindows()
 
-track()
+matrix_coefficients= [ 1.4109783873986503e+03, 0., 9.5874222351204332e+02, 0.,
+       1.4102028573625632e+03, 5.4948497502265627e+02, 0., 0., 1. ]
+distortion_coefficients= [ 3.9039330491598877e-02, -2.2797071348585993e-01,
+       -1.1703590104003828e-03, -1.6147474598345472e-04,
+       2.4035667750020775e-01 ]
+track(matrix_coefficients, distortion_coefficients)
