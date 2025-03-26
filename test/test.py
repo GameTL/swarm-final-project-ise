167097a6fba56a906bac75b8da01de3adc9f253d
Ting-YiL <lin13445@gmail.com>
2025-03-26 17:40:49 +0700

    marker tracked

--- test/test.py
+++ test/test.py
@@ -1,37 +1,85 @@
 import numpy as np
 import cv2
-import cv2.aruco as aruco
+import time
+
+ARUCO_DICT = {
+    "DICT_6X6_250": cv2.aruco.DICT_6X6_250
+    
+} 
+
+prev_frame_time = 0
+new_frame_time = 0
+
+
+def aruco_display(corners, ids, rejected, image):
+    if len(corners) > 0:
+        
+        ids = ids.flatten()
+        
+        for (markerCorner, markerID) in zip(corners, ids):
+            
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+            
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+            cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+            cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+            cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+            cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+            
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+            
+            cv2.putText(image, str(markerID),(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 2)
+            print("[Inference] ArUco marker ID: {}".format(markerID))
+            
+    return image
+
+# img = cv2.imread('./marker/marker_with_bg.png', 1) 
+
 cap = cv2.VideoCapture(0)  # Get the camera source
 
-def track(matrix_coefficients, distortion_coefficients):
-    while True:
-        ret, frame = cap.read()
-        # operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Change grayscale
-        aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)  # Use 5x5 dictionary to find markers
-        parameters = aruco.DetectorParameters_create()  # Marker detection parameters
-        # lists of ids and the corners beloning to each id
-        corners, ids, rejected_img_points = aruco.detectMarkers(gray, aruco_dict,
-                                                                parameters=parameters,
-                                                                cameraMatrix=matrix_coefficients,
-                                                                distCoeff=distortion_coefficients)
-        if np.all(ids is not None):  # If there are markers found by detector
-            for i in range(0, len(ids)):  # Iterate in markers
-                # Estimate pose of each marker and return the values rvec and tvec---different from camera coefficients
-                rvec, tvec, markerPoints = aruco.estimatePoseSingleMarkers(corners[i], 0.02, matrix_coefficients,
-                                                                           distortion_coefficients)
-                (rvec - tvec).any()  # get rid of that nasty numpy value array error
-                aruco.drawDetectedMarkers(frame, corners)  # Draw A square around the markers
-                aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)  # Draw Axis
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
-        # Wait 3 milisecoonds for an interaction. Check the key and do the corresponding job.
-        key = cv2.waitKey(3) & 0xFF
-        if key == ord('q'):  # Quit
-            break
-    
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+while True:
+    ret, frame = cap.read()
 
-track()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    aruco_type = ["DICT_6X6_250"]
+    for i in aruco_type:
+        arucoDict = cv2.aruco.getPredefinedDictionary(ARUCO_DICT[i])
+
+        arucoParams = cv2.aruco.DetectorParameters()
+
+        corners, ids, rejected = cv2.aruco.ArucoDetector(arucoDict, arucoParams).detectMarkers(gray)
+
+        detected_markers = aruco_display(corners, ids, rejected, frame)
+
+    new_frame_time = time.time() 
+    fps = 1/(new_frame_time-prev_frame_time) 
+    prev_frame_time = new_frame_time 
+  
+    # converting the fps into integer 
+    fps = int(fps) 
+  
+    # converting the fps to string so that we can display it on frame 
+    # by using putText function 
+    fps = str(fps) 
+  
+    # putting the FPS count on the frame 
+    cv2.putText(detected_markers, f"fps: {fps}", (7, 70), cv2.FONT_HERSHEY_SIMPLEX , 1, (0, 0, 0), 3, cv2.LINE_AA) 
+
+    cv2.imshow("Image", detected_markers)
+    
+    key = cv2.waitKey(3) & 0xFF
+    if key == ord('q'):  # Quit
+            break
+
+
+cap.release()
+cv2.destroyAllWindows()
