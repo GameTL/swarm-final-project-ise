a65b0d712cfbfbeaa462d96c26840cd3146963f2
Game Ubuntu VM <limsila.limsila@gmail.com>
2025-03-30 20:19:45 +0700

    updated

--- ros2_ws/src/collective_transport/collective_transport/collective_transport.py
+++ ros2_ws/src/collective_transport/collective_transport/collective_transport.py
@@ -3,27 +3,103 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from datetime import datetime
-from .submodules.dynamixel_class import DynamixelInterface
+# from .submodules.dynamixel_class import DynamixelInterface
 import os
-
+import yasmin
+from yasmin import State
+from yasmin import Blackboard
+from yasmin import StateMachine
+from yasmin_ros import set_ros_loggers
+from yasmin_viewer import YasminViewerPub
 # Get the environment variable
 ROBOT_ID: int = int(os.environ.get('ROBOT_ID'))
 
 # Print the value
-print(f"Robot ID: {str(robot_id)}")
+print(f"Robot ID: {str(ROBOT_ID)}")
 
+# Define the FooState class, inheriting from the State class
+class FooState(State):
+    """
+    Attributes:
+        counter (int): Counter to track the number of executions of this state.
+    """
+
+    def __init__(self) -> None:
+        """
+        Outcomes:
+            outcome1: Indicates the state should continue to the Bar state.
+            outcome2: Indicates the state should finish execution and return.
+        """
+        super().__init__(["outcome1", "outcome2"])
+        self.counter = 0
+
+    def execute(self, blackboard: Blackboard) -> str:
+        """
+        Executes the logic for the Foo state.
+
+        Args:
+            blackboard (Blackboard): The shared data structure for states.
+
+        Returns:
+            str: The outcome of the execution, which can be "outcome1" or "outcome2".
+
+        Raises:
+            Exception: May raise exceptions related to state execution.
+        """
+        yasmin.YASMIN_LOG_INFO("Executing state FOO")
+        time.sleep(3)  # Simulate work by sleeping
+
+        if self.counter < 3:
+            self.counter += 1
+            blackboard["foo_str"] = f"Counter: {self.counter}"
+            return "outcome1"
+        else:
+            return "outcome2"
+        
+        
 def main(args=None):
-    # launch odom and localization of the ROBOT_ID
-    # launch vision 
-    # launch comms of ROBOT_ID
-    rclpy.init(args=args)
+    yasmin.YASMIN_LOG_INFO("yasmin_frfr")
     
+
+    
+    rclpy.init(args=args)
+    # Set ROS 2 loggers
+    set_ros_loggers()
+    # Create a finite state machine (FSM)
+    sm = StateMachine(outcomes=["outcome4"])
+        # Create a finite state machine (FSM)
+    sm = StateMachine(outcomes=["outcome4"])
+
+    # Add states to the FSM
+    sm.add_state(
+        "INIT",
+        Init(),
+        transitions={
+            "outcome1": "BAR",
+            "outcome2": "outcome4",
+        },
+    )
+    sm.add_state(
+        "BAR",
+        BarState(),
+        transitions={
+            "outcome3": "FOO",
+        },
+    )
+
+    # Publish FSM information for visualization
+    YasminViewerPub("yasmin_demo", sm)
+    
+    # Execute the FSM
     try:
-        rclpy.spin()
+        outcome = sm()
+        yasmin.YASMIN_LOG_INFO(outcome)
     except KeyboardInterrupt:
-        pass
-    finally:
-        x_drive_controller.destroy_node()
+        if sm.is_running():
+            sm.cancel_state()
+
+    # Shutdown ROS 2 if it's running
+    if rclpy.ok():
         rclpy.shutdown()
 
 if __name__ == '__main__':
--- ros2_ws/src/collective_transport/setup.py
+++ ros2_ws/src/collective_transport/setup.py
@@ -7,11 +7,10 @@ setup(
     name=package_name,
     version='0.0.1',
     packages=[package_name, submodules],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    # data_files=[
+    #     ('share/ament_index/resource_index/packages'),
+    #     ('share/' + package_name, ['package.xml']),
+    # ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Your Name',
